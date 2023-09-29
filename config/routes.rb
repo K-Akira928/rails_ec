5a5483acd550504c99d32f02a534699549e17436
K-Akira928 <K.AkiraRei928@gmail.com>
2023-09-30 08:53:07 +0900

    routes.rbのresourcesとrootにproductsを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :tasks
+  resources :products, only: %i[index show]
+  root to: 'products#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
