a6953b2b7be6aecfa19c56715b75d958eaab6951
K-Akira928 <K.AkiraRei928@gmail.com>
2023-09-30 09:48:31 +0900

    Products#showに商品詳細機能の実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,9 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find(params[:id])
+    @related_products = Product.order(id: :DESC)
+  end
 end
