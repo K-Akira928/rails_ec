85520a3875fab08acaa379c3510d7298e6d7ed2c
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-01 18:14:03 +0900

    db登録用データの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# 画像入りの製品10件を登録
+10.times do |n|
+  image_path = Rails.root.join("app/assets/images/#{n + 1}.jpg")
+  Product.create!(
+    name: "テスト商品#{n + 1}",
+    price: 5000,
+    description: "テスト商品#{n + 1}の説明です",
+    image: ActiveStorage::Blob.create_and_upload!(io: File.open(image_path), filename: File.basename(image_path))
+  )
+end
+
+# 画像なし製品1件を登録
+Product.create!(
+  name: '画像なしテスト商品1',
+  price: 6000,
+  description: '画像なしテスト商品1の説明です'
+)
