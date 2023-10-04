f3236e845734d8f6a8cee1cbd117b4f22102f56e
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-04 12:52:02 +0900

    controller Admin::Productsを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :products, except: %i[show]
+  end
   resources :products, only: %i[index show]
   root to: 'products#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
---
+++ spec/requests/admin/product_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "Admin::Products", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/admin/product/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/admin/product/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /edit" do
+    it "returns http success" do
+      get "/admin/product/edit"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/admin/product/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
