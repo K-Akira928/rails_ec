74b797d3b4379cf4dcd94b1d1edd8315e8bbce52
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-05 17:52:29 +0900

    admin/edit 商品編集画面を作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -4,6 +4,18 @@ class Admin::ProductsController < ApplicationController
   end
 
   def edit
-    
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    product = Product.find(params[:id])
+    product.update(product_params)
+    redirect_to admin_products_path, notice: "商品ID:#{product.id} #{product.name} の編集を完了しました"
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :price, :description, :image)
   end
 end
