192f15970da9802fd771cca41b085bda50bc0dfb
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-05 18:15:28 +0900

    admin/new 商品作成画面を作成

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -3,6 +3,14 @@ class Admin::ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+  end
+
+  def create
+    product = Product.create(product_params)
+    redirect_to admin_products_path, notice: "商品ID:#{product.id} #{product.name} の作成を完了しました"
+  end
+
   def edit
     @product = Product.find(params[:id])
   end
