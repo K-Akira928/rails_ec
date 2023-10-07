63020cd8dfdc4b5636f33d90eeaad50de37f7e6e
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-07 21:17:59 +0900

    ログアウト機能実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path, notice: 'ログアウトしました'
+  end
+
   private
 
   def session_params
