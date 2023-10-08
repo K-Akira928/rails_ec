5fa600be7a9e8dfd80aea739057ce5be59a875f0
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-08 22:19:21 +0900

    basic認証に変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  helper_method :current_user
-  before_action :login_required
+  before_action :basic_auth
 
   private
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def login_required
-    redirect_to login_path unless current_user
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['BASIC_AUTH_USER'] && password == ENV['BASIC_AUTH_PASSWORD']
+    end
   end
 end
