0af18fbf976785b67ed4b32847370eef77826609
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-13 14:29:04 +0900

    products#show からカートに追加できるように実装

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -5,8 +5,12 @@ class CartsController < ApplicationController
 
   def update
     product_in_cart_ids = []
+
     session[:product_in_cart_id] ||= product_in_cart_ids
-    session[:product_in_cart_id].push(params[:id])
-    redirect_to root_path
+
+    num_of_items = params[:num_of_items].to_i || 1
+
+    num_of_items.times { session[:product_in_cart_id].push(params[:id]) }
+    redirect_to request.referer
   end
 end
