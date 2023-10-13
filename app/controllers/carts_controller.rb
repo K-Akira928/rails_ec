fb7e98251b1f86fde12b0e318ff464e4f6ca0335
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-13 15:49:38 +0900

    products#index からの商品追加が正しくできるように修正

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -10,7 +10,7 @@ class CartsController < ApplicationController
 
     session[:product_in_cart_id] ||= product_in_cart_ids
 
-    num_of_items = params[:num_of_items].to_i || 1
+    num_of_items = params[:num_of_items].to_i
 
     num_of_items.times { session[:product_in_cart_id].push(params[:id]) }
     redirect_to request.referer
