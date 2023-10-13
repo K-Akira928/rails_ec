9eac8fb0d75ca114108f3968a3980202b0608d3e
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-13 15:56:04 +0900

    carts#destory へのルート追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :carts, only: %i[index update]
+  resources :carts, only: %i[index update destroy]
 
   namespace :admin do
     resources :products, except: %i[show]
