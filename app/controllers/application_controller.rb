74f9335e233d2450c47dbc90d8556a5f3c353d2a
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-13 22:01:28 +0900

    headerのカート個数を表示できるように実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :num_of_products_in_current_cart
+
+  private
+
+  def num_of_products_in_current_cart
+    product_in_cart_ids = []
+    session[:product_in_cart_id] ||= product_in_cart_ids
+
+    @product_in_carts = session[:product_in_cart_id].map do |prodcut_id|
+      Product.find_by(id: prodcut_id)
+    end
+  end
 end
--- app/controllers/cart/sessions_controller.rb
+++ app/controllers/cart/sessions_controller.rb
@@ -1,15 +1,8 @@
 class Cart::SessionsController < ApplicationController
   def index
-    @product_in_carts = session[:product_in_cart_id].map do |prodcut_id|
-      Product.find_by(id: prodcut_id)
-    end
   end
 
   def update
-    product_in_cart_ids = []
-
-    session[:product_in_cart_id] ||= product_in_cart_ids
-
     num_of_items = params[:num_of_items].to_i
 
     num_of_items.times { session[:product_in_cart_id].push(params[:id]) }
