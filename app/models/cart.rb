baf31e2d0e20575b35fabde13c5a0689e3fcf788
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-19 02:01:18 +0900

    scope :product_per_groups の記述をインスタンスメソッド self.product_per_groups に変更

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,7 +5,8 @@ class Cart < ApplicationRecord
   has_many :products, through: :cart_products
 
   scope :amount, ->(id) { find_by(id:).products.sum(&:price) }
-  scope :product_per_groups, lambda { |id|
+
+  def self.product_per_groups(id)
     find_by(id:).products.group(:product_id).count.transform_keys! { |k| Product.find(k) }
-  }
+  end
 end
