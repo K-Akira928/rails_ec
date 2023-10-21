8b132148bdc25eff42fd8629cdf734ef8a63176b
K-Akira928 <K.AkiraRei928@gmail.com>
2023-10-21 20:19:26 +0900

    論理削除実装に伴い商品削除、更新時の処理、表示方法を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :set_cart
   before_action :require_cart
   helper_method :current_products_in_cart
 
+  include CartsConcern
   include ProductsCart::CartsHelper
   include ProductsCart::ProductsHelper
 end
