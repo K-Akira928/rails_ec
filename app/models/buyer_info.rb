4047eeeab54132ad71640553d9e076adec4d106a
K-Akira928 <K.AkiraRei928@gmail.com>
2023-11-01 01:16:05 +0900

    validatesのemailとpost_codeに正規表現を追加

--- app/models/buyer_info.rb
+++ app/models/buyer_info.rb
@@ -1,15 +1,18 @@
 class BuyerInfo < ApplicationRecord
   has_many :purchase_details, dependent: :destroy
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_POST_CODE_REGEX = /\A[0-9]{3}-[0-9]{4}\z/i
+
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :user_name, presence: true, length: { maximum: 15 }
-  validates :email, presence: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   validates :address_first, presence: true
   validates :address_second, presence: true
   validates :country, presence: true
   validates :prefecture, presence: true
-  validates :post_code, presence: true
+  validates :post_code, presence: true, format: { with: VALID_POST_CODE_REGEX }
   validates :card_holder, presence: true
   validates :card_number, presence: true, length: { minimum: 14, maximum: 16 }
   validates :cvv, presence: true, length: { maximum: 3 }
