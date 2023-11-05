26d5d1e2ee5783f7ed0463c821568fb1d0018e0c
K-Akira928 <K.AkiraRei928@gmail.com>
2023-11-05 17:54:15 +0900

    PromotionCodesに論理削除カラムを追加

---
+++ db/migrate/20231105085313_add_discarded_at_to_promotion_codes.rb
@@ -0,0 +1,6 @@
+class AddDiscardedAtToPromotionCodes < ActiveRecord::Migration[7.0]
+  def change
+    add_column :promotion_codes, :discarded_at, :datetime
+    add_index :promotion_codes, :discarded_at
+  end
+end
