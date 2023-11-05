e65bd4d328ed552287620b085af4aa695bab5231
K-Akira928 <K.AkiraRei928@gmail.com>
2023-11-05 18:11:58 +0900

    PromotionCodesにvalidatesを設定

--- app/models/promotion_code.rb
+++ app/models/promotion_code.rb
@@ -1,2 +1,5 @@
 class PromotionCode < ApplicationRecord
+  VALID_CODE_REGEX = /\A^[0-9a-zA-Z]*$\z/i
+  validates :code, presence: true, length: { is: 7 }, format: { with: VALID_CODE_REGEX }
+  validates :discount, presence: true, numericality: { greater_than_or_equal_to: 100, less_than_or_equal_to: 1000 }
 end
